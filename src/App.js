66c914698e62389c7788a54fa2042af729d6bd7c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-07 08:26:21 +0100

    created App component

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+//styles
+import './App.css'
+
+//pages and components
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/'>
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
