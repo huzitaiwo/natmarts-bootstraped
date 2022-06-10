5f566f52d9a974c8d77f393d93447e29d4fb902f
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-10 02:35:37 +0100

    creating footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,21 @@
+//images
+import locationIcon from '../assets/location_icon.svg'
+import callIcon from '../assets/call_icon.svg'
+import mailIcon from '../assets/mail_icon.svg'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="bg-primary text-white">
+      <div className="container">
+        <h3 className="h3 text-uppercase">Import and Export</h3>
+        <div className="row">
+            <div className="col-md-4">
+                <div className="address">
+                  <img src={locationIcon} alt="location" />
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
