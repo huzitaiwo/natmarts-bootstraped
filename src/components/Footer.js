25ce267137d09d07e2ce7fd19da3c9800ecc2bc3
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-10 02:46:03 +0100

    creating footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom'
+
 //images
 import locationIcon from '../assets/location_icon.svg'
 import callIcon from '../assets/call_icon.svg'
 import mailIcon from '../assets/mail_icon.svg'
+import facebookIcon from '../assets/facebook_icon.svg'
+import whatsappIcon from '../assets/whatsapp_icon.svg'
+import instagramIcon from '../assets/insta_icon.svg'
+import twitterIcon from '../assets/twitter_icon.svg'
+import linkedinIcon from '../assets/linkedin_icon.svg'
 
 export default function Footer() {
   return (
@@ -27,6 +34,17 @@ export default function Footer() {
                     <p>+91 9969922348</p>
                   </div>
                 </div>
+                <h6 className='h6'>Follow us</h6>
+                <ul className="unstyled-list">
+                  <li><Link to='/'><img src={facebookIcon} alt="facebook" /></Link></li>
+                  <li><Link to='/'><img src={whatsappIcon} alt="whatsapp" /></Link></li>
+                  <li><Link to='/'><img src={instagramIcon} alt="instagram" /></Link></li>
+                  <li><Link to='/'><img src={twitterIcon} alt="twitter" /></Link></li>
+                  <li><Link to='/'><img src={linkedinIcon} alt="linkedin" /></Link></li>
+                </ul>
+            </div>
+            <div className="col-md-4">
+                <h6 className="h6">Our Location</h6>
             </div>
         </div>
       </div>
