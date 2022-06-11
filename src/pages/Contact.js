91b038ca2391978e9c2d91e21c4c65848a26d95e
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-11 14:25:21 +0100

    populating contact page content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,24 @@
 //images
 import banner from '../assets/hero_image.png'
+import locationIcon from '../assets/location_icon.svg'
+import callIcon from '../assets/call_icon.svg'
+import mailIcon from '../assets/mail_icon.svg'
+
+//components
 import BannerSM from '../components/BannerSM'
 
 export default function Contact() {
   return (
     <div>
       <BannerSM src={banner} content="Contact Us" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5">
+            <h3 className="h3 text-uppercase">Contact us</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
