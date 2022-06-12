87b5d8e8fa9040d51adab9b6d02d8af0000de381
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-12 11:29:56 +0100

    finihsing homepage

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <div className="col-md-5 col-xl-6 pb-4 pb-md-0">
           <img className='img-fluid' src={aboutImage} alt="what we do" />
         </div>
-        <div className="col-md-7 col-xl-6">
+        <div className="col-md-7 col-xl-6 text-justify">
           <p className='text-justify'>Unique Groups is the leading Foodstuff business operating and Facility Service in Middle East and India. We are importing and exporting all kinds of agricultural products from throughout the world. The Group's core business segment: Foodstuff trading at Dubai, Export and Import business at India, and Facility Management business. Unique has successfully leveraged its brand building and market development skills along with proven foodstuff industry experience to drive sustainable profitable growth within the Middle East and Asian regions.</p>
         </div>
       </div>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <div className='position-relative'>
       <img className='img-fluid' src={banner} alt="banner" />
-      <div className="container position-absolute top-15 start-0 text-white mt-2 mt-lg-5 ms-lg-5">
+      <div className="container position-absolute top-15 text-white mt-2 mt-lg-5 ms-lg-5">
         <h3 className='m-0'>Making Transport</h3>
         <h1 className='m-0'>FAST & SAFE</h1>
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean nascetur ridiculus mus dolor sit amet, consectetu.</p>
