60bbe5e526509bc0529c1c3f9e44065e2da53fb7
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-12 14:45:52 +0100

    populating about us page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,13 @@ import banner from '../assets/hero_image.png'
 
 //components
 import BannerSM from '../components/BannerSM'
+import Aboutus from '../components/About'
 
 export default function About() {
   return (
     <div>
       <BannerSM src={banner} content="About Us" />
+      <Aboutus />
     </div>
   )
 }
