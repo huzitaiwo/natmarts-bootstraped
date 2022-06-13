1e50caabcfda90e07b9d6ba6d6733b5fbfaec52d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-13 12:15:00 +0100

    adjusting banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,10 @@ import banner from '../assets/hero_image.png'
 
 export default function Banner() {
   return (
-    <div className='text-white' style={{ backgroundImage: `url(${banner})`, height:"600px"}}>
-      {/* <img className='img-fluid' src={banner} alt="banner" /> */}
+    <div className='text-white h-full' style={{
+      backgroundImage: `url(${banner})`, 
+      height:"500px"
+      }}>
       <div className="container">
         <h3 className='m-0'>Making Transport</h3>
         <h1 className='m-0'>FAST & SAFE</h1>
