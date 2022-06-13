152349dddb1c31899d22419376c1122893286284
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-13 15:19:24 +0100

    populating about us page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,23 +15,27 @@ export default function About() {
       <Aboutus />
       <div className="bg-info">
         <h3 className="h3 text-uppercase text-primary">Why Choose Us</h3>
-        <div className="row jusify-content-between">
+        <div className="row justify-content-between">
           <div className="col-md-5">
             <p>Unique Groups is the leading Foodstuff business operating and Facility Service in Middle East and India. We are importing and exporting all kinds of agricultural products from throughout the world. The Group's core business segment: Foodstuff trading at Dubai, Export and Import business at India, and Facility Management business. Unique has successfully leveraged its brand building and market development skills along with proven foodstuff industry experience to drive sustainable profitable growth within the Middle East and Asian regions.</p>
           </div>
           <div className="col-md-5">
-            <div className="row">
+            <div className="row align-items-between">
               <div className="col-md-6">
                 <img src={deliveryIcon} alt="delivery" />
+                <p>Fast Delivery</p>
+              </div>
+              <div className="col-md-6">
+                <img src={secureIcon} alt="secured" />
+                <p>Secured</p>
               </div>
               <div className="col-md-6">
                 <img src={deliveryIcon} alt="delivery" />
+                <p>Reasonable Price</p>
               </div>
               <div className="col-md-6">
-                <img src={deliveryIcon} alt="delivery" />
-              </div>
-              <div className="col-md-6">
-                <img src={deliveryIcon} alt="delivery" />
+                <img src={cupIcon} alt="cup" />
+                <p>Reliable</p>
               </div>
             </div>
           </div>
