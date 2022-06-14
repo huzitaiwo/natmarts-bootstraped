1ca3f02af5270159facb9257d880395c8304ed16
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-14 13:37:00 +0100

    finishing contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -30,7 +30,7 @@ export default function Contact() {
             </div>
           </div>
           <div className="col-md-5">
-            <h3 className="h4 text-uppercase fw-bolder text-primary mb-3">Send us a messsage</h3>
+            <h3 className="h4 text-uppercase fw-bolder text-primary mt-5 mt-md-0 mb-3">Send us a messsage</h3>
             <p className='mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolor.</p>
             <form className='row g-4 mb-3'>
               <div className="col-md-6">
