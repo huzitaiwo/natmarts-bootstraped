e937a06db2d6d35f432b26367e34b593a9c1928b
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-06-15 13:46:52 +0100

    finishing about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ export default function About() {
   return (
     <div className="container">
       <div className='row py-3 py-md-5 align-items-center'>
-        <div className="col-md-5 col-xl-6 pb-4 pb-md-0">
+        <div className="col-xl-5 pb-4 pb-xl-0">
           <img className='img-fluid' src={aboutImage} alt="what we do" />
         </div>
-        <div className="col-md-7 col-xl-6 text-justify">
+        <div className="col-xl-7 text-justify">
           <p className='text-justify'>Unique Groups is the leading Foodstuff business operating and Facility Service in Middle East and India. We are importing and exporting all kinds of agricultural products from throughout the world. The Group's core business segment: Foodstuff trading at Dubai, Export and Import business at India, and Facility Management business. Unique has successfully leveraged its brand building and market development skills along with proven foodstuff industry experience to drive sustainable profitable growth within the Middle East and Asian regions.</p>
         </div>
       </div>
